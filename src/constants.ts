67266493729bcd2b44bfd52efa6c1d642e5c2f87
enderschesi2021 <64193056+enderschesi@users.noreply.github.com>
2022-04-09 21:37:15 +0200

    Changed to my Domain

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = "https://api.lanyard.rest/v1";
-export const WEBSOCKET_URL = "wss://api.lanyard.rest/socket";
+export const API_URL = "https://lanyard.enderschesi.me/v1";
+export const WEBSOCKET_URL = "wss://lanyard.enderschesi.me/socket";
 export const HEARTBEAT_INTERVAL = 30_000;
